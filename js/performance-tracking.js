69dc9578ad63cb32882fd40471cfb18d2006255d
Sdavidson5 <86085106+Sdavidson5@users.noreply.github.com>
2024-06-03 21:51:25 -0400

    Trying to figure out js for notes list

--- js/performance-tracking.js
+++ js/performance-tracking.js
@@ -1,10 +1,31 @@
-const notes = [];
+const notesList = ['Hendricken Soccer is the best'];
 
-function addToNotes(){
-    const inputElement = document.querySelector('.js-input-notes');
-    console.log(inputElement);
-    const note = inputElement.value;
+renderNotesList();
 
-    notes.push(note);
-    console.log(notes)
+function renderNotesList(){
+    let notesListHTML = '';
+
+    for (let i = 0; i < notesList.length; i++) {
+        const notes = notesList[i];
+        const html = `<p>${notes}</p>`;
+        notesListHTML += html;
+    }
+    console.log(notesListHTML);
+
+    document.querySelector(`.js-notes-list`).
+        innerHTML = notesListHTML;
+
 }
+function addToNotes(){
+        const inputElement = document.querySelector(`.input-notes`);
+        const note = inputElement.value;
+
+        notesList.push(note);
+        console.log(notesList);
+
+        inputElement.value = ``;
+
+        renderNotesList();
+}
+
+
