a7d396b7f5d7bb530a0732c4ded78b0f3a53b5b1
Sdavidson5 <86085106+Sdavidson5@users.noreply.github.com>
2024-07-28 22:42:14 -0400

    Array Work

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
-const num = [10,20,30];
-num[2] = 99;
-console.log(num);
-function getLastVaule(array)
+const x = [5,10,30,50,99];
+function getLastVaule(){
+    console.log(x);
+    const lastIndex = x[x.length-1];
+    return lastIndex
+}
+
+getLastVaule(x)
 
 
 
