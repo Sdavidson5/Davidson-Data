dfe1e7c2d3233d710d594a5d9685071d079304c0
Sdavidson5 <86085106+Sdavidson5@users.noreply.github.com>
2024-08-08 22:36:05 -0400

    JS console work

--- js/index.js
+++ js/index.js
@@ -1,18 +1,4 @@
 
-let soup = 10;
-let burgers = 8;
-let ic = 5;
-
-let meal = 1*soup + 3*burgers +1*ic;
-
-console.log(meal/3);
-    
-let toaster = 18.5;
-let shirts = 7.5;
-
-let total = toaster+shirts;
-console.log(total*1.2);
-
 
 
 
