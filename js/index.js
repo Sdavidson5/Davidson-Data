95c109883c52cbfce7dadf41eb37d66b68a22394
Sdavidson5 <86085106+Sdavidson5@users.noreply.github.com>
2024-08-27 21:15:01 -0400

    JS work

--- js/index.js
+++ js/index.js
@@ -36,6 +36,33 @@ if (result === guess) {
     console.log('you lose')
 }
 
+function greet(name) {
+    if (name === undefined) {
+        console.log('Hi there!')
+    }
+    else return console.log(`Hello ${name}!`)
+}
+
+greet('shane');
+
+function convertToFahrenheit(degrees,unit) {
+    let f = (degrees*9/5)+32;
+    let c = (degrees-32)*5/9;
+    if (unit==='F') {
+        console.log(c)
+    } else {
+        console.log(f)
+    }
+}
+
+convertToFahrenheit(52,'F')
+
+function convertLength(length, from, to) {
+    let km = 
+}
+
+
+
 
 
 
