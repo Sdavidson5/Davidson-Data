48c28cb937be9eb4f3f6692ec4e9bc421efeb4bf
Sdavidson5 <86085106+Sdavidson5@users.noreply.github.com>
2024-08-29 21:45:57 -0400

    JS8 work

--- js/index.js
+++ js/index.js
@@ -73,26 +73,13 @@ basketball["delivery-time"] = '3 days';
 
 console.log(basketball)
 
-function comparePrice(product1,product2) {
-    const product1={
-        name:'p1',
-        price:5,
-    }
-
-    const product2={
-        name:'p2',
-        price:12,
-    }
-
-    if (product1.price>product2.price) {
-        console.log('p1 is more expensive')
+function isSameProduct(product1,product2) {
+    if (product1.name===product2.name && product1.price===product2.price) {
+        console.log()
     } else {
-        console.log('p2 is more expensive')
+        
     }
 }
-
-comparePrice()
-
 
 
 
