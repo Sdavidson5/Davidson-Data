9554557517da69024be4d4320b859eddd8629898
Sdavidson5 <86085106+Sdavidson5@users.noreply.github.com>
2024-09-01 19:01:08 -0400

    JS dom work

--- js/test.js
+++ js/test.js
@@ -1,2 +1,9 @@
-let element = document.querySelector('.hello');
-console.log(element); 
+const element = document.querySelector('button');
+console.log(element);
+
+const element2 = document.querySelector('.bee');
+element2.textContent = '9b done!';
+
+
+
+
