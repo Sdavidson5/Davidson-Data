aa5a75b152cfc304894772ed671d192d42aca0f8
Sdavidson5 <86085106+Sdavidson5@users.noreply.github.com>
2024-09-05 22:37:30 -0400

    Cleanup

--- js/test.js
+++ js/test.js
@@ -4,6 +4,13 @@ console.log(element);
 const element2 = document.querySelector('.bee');
 element2.textContent = '9b done!';
 
+function nameInput() {
+    const inputElement = document.querySelector('.name');
+    const text= inputElement.value;
+    const nameElement = document.querySelector('.nameAnswer');
+    nameElement.textContent = `Your name is:${text}` 
+}
+
 
 
 
