27ad74d561d85720bbe8e14d67fddad83dd7e515
Sdavidson5 <86085106+Sdavidson5@users.noreply.github.com>
2024-09-10 22:20:31 -0400

    JS input work

--- js/test.js
+++ js/test.js
@@ -4,13 +4,17 @@ console.log(element);
 const element2 = document.querySelector('.bee');
 element2.textContent = '9b done!';
 
-function nameInput() {
-    const inputElement = document.querySelector('.name');
-    const text= inputElement.value;
-    const nameElement = document.querySelector('.nameAnswer');
-    nameElement.textContent = `Your name is:${text}` 
-}
 
+function nameInput() {
+    if(event.key === 'Enter') {
+        const inputElement = document.querySelector('.name');
+        const text= inputElement.value;
+        const nameElement = document.querySelector('.nameAnswer');
+        nameElement.textContent = `Your name is:${text}`
+    } else if(===''){
+    nameElement.textContent = ``;
+    }   
+}
 
 
 
