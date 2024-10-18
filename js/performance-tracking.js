d27a1c43208f4c8dfd9dc1b99356764cec4026ab
Sdavidson5 <86085106+Sdavidson5@users.noreply.github.com>
2024-10-17 21:36:20 -0400

    JS work
    JS work on adding notes for notes section

--- js/performance-tracking.js
+++ js/performance-tracking.js
@@ -1,36 +1,5 @@
-const notesList = ['Hendricken Soccer'];
+const notes =['Hendricken'];
 
-renderNotesList();
+const noteStorage = document.getElementById("notes-storage").innerHTML;
 
-function renderNotesList(){
-    let notesListHTML = '';
-
-    for (let i = 0; i < notesList.length; i++) {
-        const notes = notesList[i];
-        const html = `<div>${notes}</div>`;
-        notesListHTML += html;
-    }
-
-    document.getElementById('.js-notes-list')
-        .innerHTML = notesListHTML;
-}
-
-function addToNotes(){
-        const inputElement = document.getElementById('.input-notes');
-        const note = inputElement.value;
-
-        notesList.push(note);
-
-        inputElement.value = '';
-
-        renderNotesList();
-}
-
-
-
-for (let index = 0; index <=10; index +=2) {
-    console.log(index);
-}
-
-
-
+ 
